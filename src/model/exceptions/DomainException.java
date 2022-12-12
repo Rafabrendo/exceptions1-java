0ebc16e1d596a87765159e6a894067a22c334405
Rafael Brendo <98275054+Rafabrendo@users.noreply.github.com>
2022-12-12 15:42:34 -0300

    Update DomainException.java

--- src/model/exceptions/DomainException.java
+++ src/model/exceptions/DomainException.java
@@ -4,8 +4,8 @@ public class DomainException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	
 	public DomainException(String msg) {
-		super(msg);//Fiz isso para permitir que eu posso instanciar a minha exceção personalizada
-		//passando uma msg para ela. Essa msg fica armazenada na minha esceção
+		super(msg);//Fiz isso para permitir que eu possa instanciar a minha exceÃ§Ã£o personalizada
+		//passando uma msg para ela. Essa msg fica armazenada na minha exceÃ§Ã£o
 	}
 
 }
